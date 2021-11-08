ea0b56e6bdfe22e8aa2c01f0089f8b406d024897
alecfraser94 <68567109+alecfraser94@users.noreply.github.com>
2021-11-08 16:07:12 -0600

    Update look_at_activation_maps.py

--- look_at_activation_maps.py
+++ look_at_activation_maps.py
@@ -38,7 +38,7 @@ starting_weights=''  			#string: location of starting weights to load into the m
 data_directory='' 		        #string: location of training and validation data
 viz_layer_number=0		        #int: conv2D layer number to visualize
 filter_number=0			        #int: filter number to visualize
-img_name=''	                    #string: name of the image to visualize
+img_name=''	                      #string: name of the image to visualize
     
 def create_model(num_filters,filter_size,size,l2_reg,dropout_rate,num_dense,starting_weights,lr):
     """function which creates, loads in weights and compiles the convolutional neural network  
